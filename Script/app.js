a86ab8a54ccb998d369be9d85c80d23e9862ebc8
Jerie See Villanueva <codestackjs007@gmail.com>
2024-04-24 19:42:15 -0700

    changes

--- Script/app.js
+++ Script/app.js
@@ -1,13 +1,14 @@
 //alert ("hello");
 let jokes = document.getElementById("jokes");
-
+let rndImage = document.getElementById("rndImage");
 
 
 let jokeButton = document.getElementById("jokeButton").addEventListener("click", function(){
     getJokes();
+    changeBackground();
 });
 
-
+//Pulling API
 async function getJokes(){
     let apiResponse = await fetch("https://icanhazdadjoke.com/", {
         headers: {
@@ -18,3 +19,13 @@ async function getJokes(){
     
 };
 
+
+function changeBackground(){
+    let images = [
+        'Homer-Simpson.jpeg',
+        'HomerSimpson.jpeg',
+        'simp_homersingle08_f_hires2-2000-cf09d1b1345c4e66b57bced2bebbe492.jpg'
+    ];
+    let randomImages = images[Math.floor(Math.random() * images.length)];    
+    rndImage.src = "/Images/" + randomImages;
+}
